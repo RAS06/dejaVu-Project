d54071c73d84f0098d4d31117c0df79e0108bd55
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-15 21:12:47 -0400

    Started and finished the teleporter mechanisms.
    Object that moves the player from room to room.

--- teleporter.java
+++ teleporter.java
@@ -12,8 +12,31 @@ public class teleporter extends Actor
      * Act - do whatever the teleporter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    World target;
+    private int x;
+    private int y;
+    String dir;
     public void act()
     {
-        // Add your action code here.
+        teleport();
+    }
+    public teleporter(World targetLocation, String image, int moveToX, int moveToY){
+        target = targetLocation;
+        this.setImage(image);
+        dir = image;
+    }
+    public void teleport(){
+        if(this.isTouching(champion.class)){
+            Greenfoot.setWorld(target);
+            if(dir.equals("horizBorderControl.png")){
+                for(Actor a: target.getObjects(champion.class)){
+                    a.setLocation(champion.getChampX(), target.getHeight() - champion.getChampY());
+                }
+            } else {
+                for(Actor a: target.getObjects(champion.class)){
+                    a.setLocation(target.getWidth() - champion.getChampX(), champion.getChampY());
+                }
+            }
+        }
     }
 }
