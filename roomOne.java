b178df057e9284ae05798e912968bb3400520f70
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-16 16:00:52 -0400

    Ran into some unforseen issues pertaining to the world-changer mechanism. Made some progress on that issue, problem persists.
    I have no idea how to program.

--- roomOne.java
+++ roomOne.java
@@ -11,13 +11,15 @@ public class roomOne extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-        
+    //public static World worldOne = getWorld();
+    public static World worldTwo = new roomTwo();
     public roomOne()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(750, 750, 1);
         addObject(new champion(), 200, 200);
         addObject(new key(), 500, 500);
+        showText("roomOne", 500, 500);
         
         for(int i = -1; i < 25; i++){
             addObject(new obstructor("right", "vertBorderControl.png"), 720, i * 30);
@@ -26,7 +28,7 @@ public class roomOne extends World
             addObject(new obstructor("bottom", "horizBorderControl.png"), i * 30, 720);
         }
         for(int i = 0; i < 5; i++){
-            addObject(new teleporter(new roomTwo(),"horizBorderControl.png", 375, 720), 275 + 30 * i, 50);
+            addObject(new teleporter(worldTwo,"horizBorderControl.png", 375, 720), 275 + 30 * i, 50);
         }
     }
 }
--- roomTwo.java
+++ roomTwo.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class roomTwo extends World
 {
-
+    public static World worldOne = new roomOne();
     /**
      * Constructor for objects of class roomTwo.
      * 
@@ -18,5 +18,9 @@ public class roomTwo extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(750, 750, 1); 
         addObject(new champion(), 100, 100);
+        showText("roomTwo", 500, 500);
+        for(int i = 0; i < 5; i++){
+            addObject(new teleporter(worldOne,"horizBorderControl.png", 375, 720), 275 + 30 * i, 50);
+        }
     }
 }
