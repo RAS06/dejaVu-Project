e785abf00b3157fa75b0139cefcd1c8f52274ed2
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-27 22:30:31 -0400

    Started the dialouge query.

---
+++ chatDialougeQuerySelector.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class chatDialougeQuerySelector here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class chatDialougeQuerySelector extends Actor
+{
+    /**
+     * Act - do whatever the chatDialougeQuerySelector wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int form;
+    public void act()
+    {
+        // Add your action code here.
+    }
+    public chatDialougeQuerySelector(int formIn){form = formIn;}
+    public void showQuery(){
+        if(form == 1){
+            
+        }
+    }
+}
