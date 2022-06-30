8ecb8bc5974e2119a1666362dc31ad16dbf6d2ea
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-30 17:21:07 -0400

    Added to pregeneration.

--- colorPuzzle.java
+++ colorPuzzle.java
@@ -1,35 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class dialPuzzle here.
+ * Write a description of class colorPuzzle here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class dialPuzzle extends World
+public class colorPuzzle extends World
 {
 
     /**
-     * Constructor for objects of class dialPuzzle.
+     * Constructor for objects of class colorPuzzle.
      * 
      */
     private static boolean added = false;
-    public dialPuzzle()
+    public colorPuzzle()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(750, 750, 1);
         if(!added){
-            worldMaster.addWorld(15, this);
+            worldMaster.addWorld(16, this);
             added = true;
-            worldMaster.getWorlds().get(15).addObject(new champion(), 375, 700);
+            worldMaster.getWorlds().get(16).addObject(new champion(), 375, 700);
             Greenfoot.setWorld(worldMaster.getWorlds().get(0));
         }
-        addObject(new dial(), 150, 300);
-        addObject(new dial(), 350, 300);
-        addObject(new dial(), 550, 300);
-        //prepare();
     }
     public String toString(){
-        return "Dial Puzzle";
+        return "Color Puzzle";
     }
 }
