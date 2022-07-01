ae22044ee95a990a342541f2216254f9d4e95649
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-30 20:47:12 -0400

    Removed some whitespace.

--- roomEight.java
+++ roomEight.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class roomEight extends World
 {
-
     /**
      * Constructor for objects of class roomEight.
      * 
