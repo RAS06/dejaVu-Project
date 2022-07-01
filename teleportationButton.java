eb47e7b1700ef864b2fefffabb22089d00668736
RAS06 <rizwan.a.z.syed1996@gmail.com>
2022-06-30 20:48:03 -0400

    Implemented and added.

---
+++ teleportationButton.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class teleportationButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class teleportationButton extends Actor
+{
+    /**
+     * Act - do whatever the teleportationButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private World target;
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("b")){
+            Greenfoot.setWorld(target);
+        }
+    }
+    public teleportationButton(String filename, World initTarget){setImage(filename); target = initTarget;}
+}
